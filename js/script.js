65d8f57c765888c687c91c196514aaaf3e932789
GustavoOmai <omai.gustavoh@gmail.com>
2023-09-14 17:37:06 -0300

    Header Effect

--- js/script.js
+++ js/script.js
@@ -1,23 +1,37 @@
  function verificarPosicaoElemento() {
     const elemento = document.getElementById('logo');
     const ancoragem = document.getElementById('ancoragem');
+    const logo = document.getElementById('logosvg');
     let posicao = ancoragem.getBoundingClientRect();
-    let alturaJanela = window.innerHeight;
-    
-
-    console.log(alturaJanela);
-
-    if (posicao.top <= 20) {
+    let windowHeight  = window.innerHeight;
+    if (posicao.top <= 120) {
         elemento.classList.add('elemento-no-topo');
         elemento.classList.remove('logo-absolute');
     }
+    if (posicao.top > 120) {
 
-    console.log(posicao);
+            const scrollTop = window.scrollY;
 
-    if (ancoragem.bottom > alturaJanela) {
+            const initialSize = windowHeight /2.45;
+            const newSize = initialSize - (scrollTop * 0.4); 
+            const finalSize = newSize;
+
+            logo.style.width = `${finalSize}px`;
+            logo.style.height = `${finalSize}px`;
+
         elemento.classList.remove('elemento-no-topo');
         elemento.classList.add('logo-absolute');
     }
 }
+
+
+window.onload = function() {
+    const logo = document.getElementById('logosvg');
+
+    const sizex = window.innerHeight / 2.45;
+    logo.style.width = `${sizex}px`;
+    logo.style.height = `${sizex}px`;
+}
+  
 
 window.addEventListener('scroll', verificarPosicaoElemento);
