cbb2ca6756ce611a37888d4fe84d3b0542a35a32
GustavoOmai <omai.gustavoh@gmail.com>
2023-09-14 17:52:09 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
- function verificarPosicaoElemento() {
+window.onload = function () {
+    const logo = document.getElementById('logosvg');
+
+    const sizex = window.innerHeight / 2.45;
+    logo.style.width = `${sizex}px`;
+    logo.style.height = `${sizex}px`;
+}
+
+
+
+function verificarPosicaoElemento() {
     const elemento = document.getElementById('logo');
     const ancoragem = document.getElementById('ancoragem');
     const logo = document.getElementById('logosvg');
     let posicao = ancoragem.getBoundingClientRect();
-    let windowHeight  = window.innerHeight;
+    let windowHeight = window.innerHeight;
     if (posicao.top <= 120) {
         elemento.classList.add('elemento-no-topo');
         elemento.classList.remove('logo-absolute');
     }
     if (posicao.top > 120) {
 
-            const scrollTop = window.scrollY;
+        const scrollTop = window.scrollY;
 
-            const initialSize = windowHeight /2.45;
-            const newSize = initialSize - (scrollTop * 0.4); 
-            const finalSize = newSize;
+        const initialSize = windowHeight / 2.45;
+        const newSize = initialSize - (scrollTop * 0.4);
+        const finalSize = newSize;
 
-            logo.style.width = `${finalSize}px`;
-            logo.style.height = `${finalSize}px`;
+        logo.style.width = `${finalSize}px`;
+        logo.style.height = `${finalSize}px`;
 
         elemento.classList.remove('elemento-no-topo');
         elemento.classList.add('logo-absolute');
@@ -25,13 +35,7 @@
 }
 
 
-window.onload = function() {
-    const logo = document.getElementById('logosvg');
 
-    const sizex = window.innerHeight / 2.45;
-    logo.style.width = `${sizex}px`;
-    logo.style.height = `${sizex}px`;
-}
-  
+
 
 window.addEventListener('scroll', verificarPosicaoElemento);
